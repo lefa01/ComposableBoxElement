815164edd1a75755d71fa3d12659971c890621dc
lefa01 <lefaamani@gmail.com>
2023-10-15 15:14:44 +0400

    origin main

---
+++ app/src/main/java/com/hadf/composebox/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.hadf.composebox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hadf.composebox.ui.theme.ComposeBoxTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBoxTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .background(androidx.compose.ui.graphics.Color.Blue),
+            contentAlignment = Alignment.Center
+
+        ) {
+            Box(
+                modifier = Modifier
+                .height(50.dp)
+                .width(50.dp)
+                .background(androidx.compose.ui.graphics.Color.Green))
+            Text(text = "I Love Android", fontSize = 40.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeBoxTheme {
+        Greeting()
+    }
+}
